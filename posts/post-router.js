8956aea302ea1325c438f588bc8830feade50044
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-04-16 22:10:14 -0500

    guided project

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js"); // Connection to the DataBase
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  // get the data from the database
+  // select * from post;
+  db.select("*")
+    .from("posts") //Return a promise
+    .then((rows) => {
+      res
+        .status(200)
+        .json({
+          data: "rows",
+        })
+        .catch((error) => {
+          res.status(500).json({
+            message: "Sorry, ran into an error"
+          });
+        });
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {});
 
-});
+router.post("/", (req, res) => {});
 
-router.post('/', (req, res) => {
+router.put("/:id", (req, res) => {});
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
